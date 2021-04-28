87b50cda0b0f9abb32bc8a57e0c04f78bd8f4ad7
Supporter09 <maivannhatminh2005@gmail.com>
2021-04-28 16:03:11 +0700

    Add boostrap stuff here

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // styles
+import "assets/css/bootstrap.min.css";
+import "assets/css/paper-kit.css";
+// import "assets/css/paper-kit.min.css";
+// import "assets/css/paper-kit.css.map";
+import "assets/demo/demo.css";
 // import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
