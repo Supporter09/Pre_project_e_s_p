d50285183a1ded46e85230171ac6071d10616b2a
Supporter09 <maivannhatminh2005@gmail.com>
2021-04-28 15:53:10 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // styles
-import "bootstrap/scss/bootstrap.scss";
+// import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
 
